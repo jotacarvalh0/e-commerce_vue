b275d0ffb5a35affdf4317b5d51e57795d1ba168
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-11-30 19:25:13 -0300

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="banner pt-32">
+      <div class="container">
+        <img src="images/banner.webp" alt="Banner">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
